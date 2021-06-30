14d588ca2d011ebb2d3ccb1a223062da81bd74bc
Camilo García <72902253+camilogs1@users.noreply.github.com>
2021-06-29 21:19:19 -0500

    Final2
    Actualizacion funciones en el main

--- areas.py
+++ areas.py
@@ -3,10 +3,13 @@ from sympy import *
 import sympy
 from numpy import *
 import matplotlib.pyplot as plt
+from sympy.core import expr
 from sympy.simplify.fu import L
 sympy.init_printing()
 
+
 def puntos_corte(expre_1,expre_2,): 
+    x, y, z = symbols('x, y, z')
     expre_1 = sympify(expre_1)
     expre_2 = sympify(expre_2)
     aux_puntos = (expre_1)-(expre_2)      
@@ -14,7 +17,7 @@ def puntos_corte(expre_1,expre_2,):
     return solucion
 
 def evaluar(exp1, exp2, l1):
-
+    x, y, z = symbols('x, y, z')
     evaluar = l1 + 0.2
     eval_expre1 = exp1.evalf(subs={x: evaluar}) #evalua en la primera funcion 
     eval_expre2 = exp2.evalf(subs={x: evaluar}) # evalua en la segunda funcion
@@ -26,51 +29,33 @@ def evaluar(exp1, exp2, l1):
 
     return(exp1, exp2)     
 
-x,y, z = symbols('x, y, z')  
-print("Primera función")
-expre1 = input("Expresion en terminos de x: ")
-print("¿Desea ingresar otra funcion?")
-print("1. Si")
-print("2. No")
-resp = input("Ingrese su eleccion: ")
-if(resp == '1'):
-    print("segunda función")         
-    expre2 = input("Expresion en terminos de x: ")
-else:
-    expre2 ='0'
+def area(expre1, expre2, lim1, lim2):
+    x, y, z = symbols('x, y, z')
+    lim1=sympify(lim1)
+    lim2=sympify(lim2)
+    expre1=sympify(expre1)
+    expre2=sympify(expre2) 
 
-p_corte = puntos_corte(expre1, expre2)
-#p_corte = sympify(p_corte)
+    #ordena las funciones en mayor y menor
+    lista=evaluar(expre1, expre2, lim1)
 
-print("Los puntos de corte entre ",expre1," y ", expre2,"es: ",p_corte)
+    expre1 = lista[0]
+    expre2 = lista[1]
 
-lim1 = (input("Limite inferior: "))
-lim2 = (input("limite superior: "))
+    aux = (expre1) - (expre2)   
+    areaf = integrate(aux ,(x, lim1, lim2))
+    return expre1, expre2, areaf
 
-lim1=sympify(lim1)
-lim2=sympify(lim2)
-expre1=sympify(expre1)
-expre2=sympify(expre2) 
+def graficar(expre1, expre2, lim1, lim2):
+    x, y, z = symbols('x, y, z')
+    lim1=sympify(lim1)
+    lim2=sympify(lim2)
+    expre1=sympify(expre1)
+    expre2=sympify(expre2) 
+    h1=(lim1-10)
+    h2=(lim2+10)
 
-#ordena las funciones en mayor y menor
-lista=evaluar(expre1, expre2, lim1)
-
-expre1 = lista[0]
-expre2 = lista[1]
-
-print("La funcion mayor es: ")
-pprint(expre1)
-print("La funcion menor es: ")
-pprint(expre2)
-
-aux = (expre1) - (expre2)   
-
-print("EL AREA DE LA FUNCION= ", integrate(aux ,(x, lim1, lim2)), "U^2")
-
-h1=(lim1-10)
-h2=(lim2+10)
-
-p=plot(expre1,expre2, (x, h1, h2), legend = true, show=false)
-p[0].line_color = 'b'
-p[1].line_color = 'r'
-p.show()
+    p=plot(expre1,expre2, (x, h1, h2), legend = true, show=false)
+    p[0].line_color = 'b'
+    p[1].line_color = 'r'
+    p.show()    
--- main.py
+++ main.py
@@ -4,14 +4,8 @@ import tkinter as tk
 import tkinter
 import tkinter.font as tkFont
 from tkinter import messagebox 
-from numpy.lib import math
-from sympy import *
-import sympy
-from numpy import *
-import matplotlib.pyplot as plt
-from sympy.simplify.fu import L
-sympy.init_printing() 
-x, y, z = symbols('x, y, z')
+from areas import *
+from tkinter import messagebox
 
 def main():
     ventana= Tk()
@@ -35,99 +29,106 @@ def main():
     pes2 = tkinter.Frame(notebook,background="white")
     pes3 = tkinter.Frame(notebook,background="white")
     notebook.add(pes0, text='Integrales')
+
     #Parte Area   
     notebook.add(pes1, text='Área')
     fun1=tk.Label(pes1,text="Ingrese la funcion en terminos de x: ", width="35", height="1", font=("Helvetica 14"), bg='white')
-    fun1.grid(row=2, column=0)
+    fun1.grid(row=2, column=0, pady=4)
     caj1 = Entry(pes1, width= 45, font = ("Helvetica 16"), highlightbackground='#007b99', highlightcolor='#f39200', highlightthickness=3)
-    caj1.grid(row=2, column=1)
+    caj1.grid(row=2, column=1, pady=4)
     #accion boton
     def sefu():
         fun2.grid(row=4, column=0)
         caj2.grid(row=4,column=1)
+
     bot2fun = tk.Button(pes1, text="Añadir segunda funcion", width = "20", height = "1", command=sefu 
     , font = ("Helvetica 14"),foreground="white", bg='#007b99', activebackground='white', activeforeground='#007b99')
-    bot2fun.grid(row=3,column=0)
+    bot2fun.grid(row=3,column=0, pady= 6)
     fun2=tk.Label(pes1,text="Ingrese la funcion en terminos de x: ", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
     caj2 = Entry(pes1, width= 45, font= ("Calibri 16"), highlightbackground='#007b99'
     , highlightcolor='#f39200', highlightthickness=3)
     #accion boton
-    def puntos_corte(): 
-        expre1=caj1.get()
-        expre2=caj2.get()
-        if not expre2:
-            expre2 = 0
-        expre1 = sympify(expre1)
-        expre2 = sympify(expre2)
-        aux_puntos = (expre1)-(expre2)      
-        solucion = solve(aux_puntos, dict=False)  
-        pcorte = tk.Label(pes1, text="Los puntos de corte son: " + str(solucion), width = "35", height = "1"
+    def pun_cor():
+        e1 = caj1.get()
+        if not e1:
+            messagebox.showerror("Error", "Ingrese una funcion") 
+        e2 = caj2.get()
+        if not e2:
+            e2 = 0
+        aux1 = puntos_corte(e1, e2)
+        resultPc = tk.Label(pes1, text="Los puntos de corte son: " + str(aux1), width = "35", height = "1"
         , font = ("Helvetica 14"), bg='white')
-        pcorte.grid(row=6,column=0)
+        resultPc.grid(row=6,column=0)
 
-    botpun = tk.Button(pes1, text="Calcular puntos de corte", width = "20", height = "1", command = puntos_corte
-     , font = ("Helvetica 14") ,foreground="white", bg='#007b99', activebackground='white', activeforeground='#007b99')
-    botpun.grid(row=5, column=0)
+    botpun = tk.Button(pes1, text="Calcular puntos de corte", width = "20", height = "1", command = pun_cor
+    , font = ("Helvetica 14") ,foreground="white", bg='#007b99', activebackground='white', activeforeground='#007b99')
+    botpun.grid(row=5, column=0, pady= 8)
     liminfe = tk.Label(pes1, text="Ingrese limite inferior: ", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
     liminfe.grid(row=7, column=0)
     caj3 = Entry(pes1,width= 45, font= ("Calibri 16"), highlightbackground='#007b99', highlightcolor='#f39200', highlightthickness=3)
-    caj3.grid(row=7, column=1)
+    caj3.grid(row=7, column=1, pady= 9)
     limsupe = tk.Label(pes1, text="Ingrese limite superior: ", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
     limsupe.grid(row=10, column=0)
     caj4 = Entry(pes1,width= 45, font= ("Calibri 16"), highlightbackground='#007b99', highlightcolor='#f39200', highlightthickness=3)
-    caj4.grid(row=10, column=1)
+    caj4.grid(row=10, column=1, pady= 11)
     #accion boton
-    #grafica
-    def grafica(expre1, expre2, lim1, lim2):  #sin sombrear
-        expre1=sympify(expre1)       
-        expre2=sympify(expre2)  
-        h1=(lim1-2)
-        h2=(lim2+2)
-        p=plot(expre1,expre2, (x, h1, h2), legend = true, show=false)
-        p[0].line_color = 'b'
-        p[1].line_color = 'r'
-        p.show()  
-    #evaluar funcion mayor
-    def evaluar(expre1, expre2, l1):
-        expre1 = sympify(expre1)
-        expre2 = sympify(expre2)
-        l1 = sympify(l1)
-        evaluar = l1 + 0.2
-        eval_expre1 = expre1.evalf(subs={x: evaluar}) #evalua en la primera funcion 
-        eval_expre2 = expre2.evalf(subs={x: evaluar}) # evalua en la segunda funcion
-        if eval_expre1 < eval_expre2:  #identifica la funcion mayor y la menor
-            aux1=expre1     
-            expre1=expre2          
-            expre2=aux1
-        return expre1, expre2
-    #calcular area
-    def area_funcion():
-        expre1=caj1.get()
-        expre2=caj2.get()
-        lim1=caj3.get()
-        lim2=caj4.get()
-        if not expre2:
-            expre2 = 0
-        lim1=sympify(lim1)
-        lim2=sympify(lim2)
-        expre1=sympify(expre1)
-        expre2=sympify(expre2)  
-        lista=evaluar(expre1, expre2, lim1)
-        expre1 = lista[0]
-        expre2 = lista[1]
-        aux = (expre1) - (expre2)   
-        area = integrate(aux ,(x, lim1, lim2)), "U^2"
-        funmay = tk.Label(pes1, text="La funcion mayor es: " + str(expre1), width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+    def are():
+        e1 = caj1.get()
+        if not e1:
+            messagebox.showerror("Error", "Ingrese una funcion") 
+        e2 = caj2.get()
+        if not e2:
+            e2 = 0
+        l1 = caj3.get()
+        l2 = caj4.get()
+        aux2 = area(e1, e2, l1, l2)
+        e1 = aux2[0]
+        e2 = aux2[1]
+        aux3 = aux2[2]
+        funmay = tk.Label(pes1, text="La funcion mayor es: " + str(e1), width = "35", height = "1", font = ("Helvetica 14"), bg='white')
         funmay.grid(row=16,column=0)
-        funmen = tk.Label(pes1, text="La funcion menor es: " + str(expre2), width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+        funmen = tk.Label(pes1, text="La funcion menor es: " + str(e2), width = "35", height = "1", font = ("Helvetica 14"), bg='white')
         funmen.grid(row=18,column=0)      
-        resulta = tk.Label(pes1, text="El area de la funcion es: " + str(area), width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+        resulta = tk.Label(pes1, text="El area de la funcion es: " + str(aux3) + " U²", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
         resulta.grid(row=20,column=0)
-        grafica(expre1, expre2, lim1, lim2)
 
-    botresult = tk.Button(pes1, text="Calcular", width = "20", height = "1", command = area_funcion, font = ("Helvetica 14")
+    botresult = tk.Button(pes1, text="Calcular", width = "20", height = "1",command = are, font = ("Helvetica 14")
     ,foreground="white", bg='#007b99', activebackground='white', activeforeground='#007b99')
-    botresult.grid(row=14,column=0)  
+    botresult.grid(row=14,column=0, pady=12)  
+    #accion boton
+    def gra():
+        e1 = caj1.get()
+        if not e1:
+            messagebox.showerror("Error", "Ingrese una funcion") 
+        e2 = caj2.get()
+        if not e2:
+            e2 = 0
+        l1 = caj3.get()
+        l2 = caj4.get()
+        graficar(e1, e2, l1, l2)
+
+    botgraf = tk.Button(pes1, text="Graficar", width = "20", height = "1",command = gra, font = ("Helvetica 14")
+    ,foreground="white", bg='#007b99', activebackground='white', activeforeground='#007b99')
+    botgraf.grid(row=24,column=0, pady=16)  
+    #accion boton
+    def eliminar():
+        caj1.delete(0, END)
+        caj2.delete(0 , END)
+        caj3.delete(0 , END)
+        caj4.delete(0 , END)
+        caj1.focus_set()
+        resultPc = tk.Label(pes1, text=" ", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+        resultPc.grid(row=6,column=0)
+        funmay = tk.Label(pes1, text=" ", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+        funmay.grid(row=16,column=0)
+        funmen = tk.Label(pes1, text=" ", width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+        funmen.grid(row=18,column=0)      
+        resulta = tk.Label(pes1, text=" " , width = "35", height = "1", font = ("Helvetica 14"), bg='white')
+        resulta.grid(row=20,column=0)
+
+    botelim = tk.Button(pes1, text="Limpiar", width = "20", height = "1", command = eliminar, font = ("Helvetica 14")
+    ,foreground="white", bg='#007b99', activebackground='white', activeforeground='#007b99')
+    botelim.grid(row=24,column=1, pady=20)  
 
     notebook.add(pes2, text='Volumen')
     notebook.add(pes3, text='About Us')
